0612c0fc639e89aa677e84c6ab7b600aa64043d6
nikkimonique <daugnik@gmail.com>
2015-10-06 13:23:05 +0800

    backend
    backend configuration

--- barcroid_backend/application/controllers/transaction.php
+++ barcroid_backend/application/controllers/transaction.php
@@ -38,9 +38,26 @@ class Transaction extends REST_Controller {
 		$id = (int) $this->get('id');
 		$this->load->model('Transaction_model','tmodel');
 		$data  = $this->tmodel->getItems($id);
+		$data2 = $this->tmodel->gettotalproducts($id);
 		$this->response($data, 200); 
 	}
 
+	public function viewitem_get(){
+		$id = (int) $this->get('id');
+		$this->load->model('Transaction_model','tmodel');
+		$data  = $this->tmodel->getItem($id);
+		$data2 = $this->tmodel->gettotalproducts($id);
+		$this->response($data, 200); 
+	}
+
+	public function totalitem_get(){
+		$id = (int) $this->get('id');
+		$this->load->model('Transaction_model','tmodel');
+		$data = $this->tmodel->gettotalproducts($id);
+		// $sum = json_decode($data);
+		var_dump($data);
+		// $this->response(array('total', $data), 200); 
+	}
 	public function selecteddetails_get(){
 		$id = (int) $this->get('id');
 		$this->load->model('Transaction_model','tmodel');
@@ -53,19 +70,100 @@ class Transaction extends REST_Controller {
 		$this->load->model('Transaction_model','tmodel');
 		$data  = $this->tmodel->getavailbleitems($name);
 		$this->response($data, 200); 
+		$this->response(array('ok' => 'Success'), 200);
 	}
 
-	public function inserttransitem_post(){
-		$data = (object) $this->post();
-		var_dump($data);
-		// $this->load->model('Transaction_model', 'tmodel');
-		// $this->tmodel->inserttransitem($data);
+	public function TransactionItem_get(){
+		date_default_timezone_set('Asia/Manila');
+		$id = (int) $this->get('item');
+		$data = array('item' => $id, 'customer_id' => 2, 'date' => date('Y-m-d'));
+		$this->load->model('Transaction_model', 'tmodel');
+		$this->tmodel->inserttransitem($data);
 	}
 
 	public function additem_post(){
 		$post_data = $this->post();
-		// var_dump($post_data);
 		$this->load->model('Transaction_model', 'tmodel');
 		$this->tmodel->insertitem($post_data);
+	}
+
+	public function ItemQty_get(){
+		$id = (int) $this->get('id');
+		$customer_id = (int) $this->get('customer_id');
+		$qty = (int) $this->get('qty');
+		if($qty == 0){
+			$finalqty = 1;
+		}else{
+			$finalqty = $qty;
+		}
+		$data = array('qty' => $finalqty);
+		$this->load->model('Transaction_model','tmodel');
+		$query = $this->tmodel->selectforqty($customer_id, $id);
+		if($query){
+			$this->tmodel->update($id, $customer_id, $data);
+			$this->response(array('Mesaage' => 'Ok'), 200);
+		}else{
+			$this->tmodel->insertonlyqty($id, $customer_id, $data);
+			$this->response(array('Mesaage' => 'Ok'), 200);
+		}
+	}
+
+	public function budgetedamnt_get(){
+		$amnt = (int) $this->get('amnt');
+		$id = 2;
+		$this->load->model('Transaction_model','tmodel');
+		$verify = $this->tmodel->verifybudget($id);
+			if($verify == NULL){ //insert
+				$data = array('budget_amnt' => $amnt, 'customer_id' => $id);
+				$this->tmodel->insertbudgetamnt($data);
+				$this->response(array('ok' => 'Success'), 200);
+			}else{ //update
+				$data = array('budget_amnt' => $amnt);
+				$this->tmodel->updatebudgetamnt($id, $data);
+				$this->response(array('ok' => 'Success'), 200);
+			}
+	}
+
+	public function stock_get(){
+		$item = (int) $this->get('item');
+		$eqty = (int) $this->get('eqty');
+		$customer_id = (int) $this->get('customer_id');
+		$this->load->model('Transaction_model','tmodel');
+		$verify = $this->tmodel->verifystock($item, $eqty, $customer_id);
+		$getarraystock = $verify[0]->items_stock;
+		$docalculation = $getarraystock - $eqty;
+		$array = array('items_stock' => $docalculation);
+		$this->tmodel->updatestock($item, $array);
+		$this->response(array('ok' => 'Success'), 200);
+	}
+
+	public function qty_get(){
+		$qty = (int) $this->get('qty');
+		$item = (int) $this->get('item');
+		$customer_id = 2;
+		$data = array('qty' => $qty);
+		$this->load->model('Transaction_model', 'tmodel');
+		$this->tmodel->editqty($item, $data, $customer_id);
+		$this->response(array('ok' => 'Success'), 200);
+	}
+
+	public function item_get(){
+		$item = $this->get('item');
+		$customer_id = 2;
+		$this->load->model('Transaction_model', 'tmodel');
+		$this->tmodel->deleteitem($item, $customer_id);
+		$this->response(array('ok' => 'Success'), 200, $item);
+	}
+
+	public function submitotcashier_get(){
+		$status = $this->get('status');
+		$date = date('Y-m-d');
+		$customer_id = 2;
+		$array = array("status" => 1);
+		$this->load->model('Transaction_model', 'tmodel');
+		$this->tmodel->updatestatus($customer_id, $date, $array);
+		$this->tmodel->deletebudget($customer_id);
+		$query = $this->tmodel->submitotcashier($status, $date);
+		var_dump($query);
 	}
 }
--- barcroid_backend/application/models/transaction_model.php
+++ barcroid_backend/application/models/transaction_model.php
@@ -20,10 +20,28 @@ class Transaction_model extends CI_Model {
 	// }
 
 	public function getItems($id){
-		// return $this->db->get('transactionview')->result_object();
-
+		$this->db->where('status', 0);
+		$this->db->order_by('brand_details', 'asc'); 
 		$this->db->where("customer_id", $id);
-		return $this->db->get('transactionview')->result_object();
+		return $this->db->get('temporaryview')->result_object();
+	}
+
+	public function getItem($id){
+		$this->db->where('status', 0);
+		$this->db->order_by('brand_details', 'asc'); 
+		$this->db->where("customer_id", $id);
+		return $this->db->get('temporaryview')->result_object();
+	}
+
+	public function deletebudget($id){
+		$this->db->where("customer_id", $id);
+		$this->db->delete('budget');
+	}
+
+	public function gettotalproducts($id){
+		$this->db->select_sum('total');
+		$this->db->where("customer_id", $id);
+		return $this->db->get('itemtotal')->result_array();
 	}
 
 	public function selecteddetails($id){
@@ -32,17 +50,87 @@ class Transaction_model extends CI_Model {
 	}
 
 	public function getavailbleitems($name){
+		$this->db->order_by('brand_details', 'asc'); 
 		$this->db->like("items_name", $name);
 		$this->db->or_like("brand_details", $name);
 		return $this->db->get('inventory')->result_object();
 	}
 
-	public function inserttransitem($data){
-		
+	public function inserttransitem($id){
+		// var_dump($id);
+		$this->db->insert('temporary', $id);
+	}
+
+	public function update($id, $customer_id, $qty){
+		$this->db->like('customer_id', $customer_id);
+		$this->db->where('item', $id);
+		return $this->db->update('temporary', $qty);
 	}
 
 	public function insertitem($data){
 		$this->db->insert('transaction', $data);
+	}
+
+	public function insertbudgetamnt($data){
+		// $this->db->where('customer_id', $id);
+		return $this->db->insert('budget', $data);
+	}
+
+	public function updatebudgetamnt($id, $data){
+		$this->db->where('customer_id', $id);
+		return $this->db->update('budget', $data);
+	}
+
+	public function verifybudget($id){
+		$this->db->where('customer_id', $id);
+		return $this->db->get('budget')->result_object();
+	}
+
+	public function verifystock($item, $eqty, $customer_id){
+		$this->db->select('items_stock');
+		$this->db->where('items_id', $item);
+		return $this->db->get('items')->result_object();
+	}
+
+	public function updatestock($item, $docalculation){
+		$this->db->where('items_id', $item);
+		return $this->db->update('items', $docalculation);
+	}
+
+	public function editqty($item, $data, $customer_id){
+		$this->db->where('item', $item);
+		$this->db->where('customer_id', $customer_id);
+		return $this->db->update('temporary', $data);
+	}
+
+	public function deleteitem($item, $customer_id){
+		$this->db->where('item', $item);
+		$this->db->where('customer_id', $customer_id);
+		return $this->db->delete('temporary');
+	}
+
+	public function selectforqty($customer_id, $id){
+		$this->db->where('customer_id', $customer_id);
+		$this->db->where('item', $id);
+		return $this->db->get('temporary');
+	}
+
+	public function insertonlyqty($id, $customer_id, $data){
+		$this->db->where('customer_id', $customer_id);
+		$this->db->where('item', $id);
+		return $this->db->insert('temporary', $data);
+	}
+
+	public function submitotcashier($status, $date){
+		$this->db->where('status', $status);
+		$this->db->where('date', $date);
+		return $this->db->get('temporary')->result_object();
+	}
+
+	public function updatestatus($customer_id, $date, $array){
+		$this->db->where('customer_id', $customer_id);
+		$this->db->where('date', $date);
+		$this->db->update('temporary', $array);	
 	}
 
 }
